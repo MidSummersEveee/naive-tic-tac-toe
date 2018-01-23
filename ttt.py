2c8360b60a580c460d07807fd9a13092635d25a6
Ian Jiang <midsummerseve1@gmail.com>
2018-01-23 17:53:12 -0500

    Version 1 Submitted Capable: Full Success Condition + HRow Counter Move

--- ttt.py
+++ ttt.py
@@ -2,81 +2,97 @@
 # Attempt on implementing a naive tic-tac-toe
 import random
 
-
 # Switching Player
 def switch_player():
 	global MoveSide
-	if(MoveSide == 'White Turn'):
-		MoveSide = 'Black Turn'
+	if(MoveSide == 'White'):
+		MoveSide = 'Black'
 	else:
-		MoveSide = 'White Turn'
-	print(MoveSide)
+		MoveSide = 'White'
+	print(f'{MoveSide} Turn')
 
 
 def place_stone(x, y, value):
+	global MoveSide
+	global Placed
+	Placed = True
 	Board[x][y] = value
+	print(f'{MoveSide[0:5]} is going to place at {x}{y}')
 
 # Attempts on Printing the Board
-# for x in range(board_height):
-# 	[print(Board[x][y], end='') if y < 2 else print(Board[x][y]) for y in range(board_width)]
 def print_board():
 	[[print(Board[x][y], end=' ') if y < 2 else print(Board[x][y]) for y in range(board_width)] for x in range(board_height)]
 
+
+def get_value_by_side(code_verse=None):
+	global MoveSide
+	if code_verse:
+		if MoveSide == 'Black':
+			return 4
+		else:
+			return 1
+	if MoveSide == 'Black':
+		return 1
+	elif MoveSide == 'White':
+		return 4
+	else:
+		print('Please Check MoveSide global')
+		return 'Please Check MoveSide global'
 
 
 # Level 1: Check Rows
 # 1.1 check horizontal
 def check_row_H():
 	global MoveSide, WinSide
-	# print('Display Horizontal Sums')
-	# [print(sum(Board[x])) for x in range(board_height)]
-	if MoveSide == 'Black Turn':
-		for x in range(board_height):
-			if sum(Board[x]) == 2:
-				print(f'Decisive Move Deteced, Horizontal Row {x} to be Completed by {MoveSide[0:5]}:')
-				Board[x] = [1, 1, 1]
-				WinSide = "Black"
-	else:
-		for x in range(board_height):
-			if sum(Board[x]) == 8:
-				print(f'Decisive Move Deteced, Horizontal Row {x} to be Completed by {MoveSide[0:5]}:')
-				Board[x] = [4, 4, 4]
-				WinSide = "White"
+	for x in range(board_height):
+		if sum(Board[x]) == 2*get_value_by_side():
+			print(f'Decisive Move Deteced, Horizontal Row {x} to be Completed by {MoveSide}:')
+			Board[x] = [2*get_value_by_side(), 2*get_value_by_side(), 2*get_value_by_side()]
+			WinSide = MoveSide
+
 
 # 1.2 check vertical
 def check_row_V():
 	global MoveSide, WinSide
-	if MoveSide == 'Black Turn':
-		for y in range(board_width):
-			if Board[0][y] + Board[1][y] + Board[2][y] == 2:
-				print(f'Decisive Move Deteced, Vertical Row {y} to be Completed by {MoveSide[0:5]}:')
-				for temp in range(3):
-					place_stone(temp, y, 1)
-				WinSide = "Black"
-	else:
-		for y in range(board_width):
-			if Board[0][y] + Board[1][y] + Board[2][y] == 8:
-				print(f'Decisive Move Deteced, Vertical Row {y} to be Completed by {MoveSide[0:5]}:')
-				for temp in range(3):
-					place_stone(temp, y, 4)
-				WinSide = "White"
+	value = get_value_by_side()
+	for y in range(board_width):
+		if Board[0][y] + Board[1][y] + Board[2][y] == 2*value:
+			print(f'Decisive Move Deteced, Vertical Row {y} to be Completed by {MoveSide}:')
+			for temp in range(3):
+				if Board[temp][y] == 0:
+					place_stone(temp, y, value)
+			WinSide = MoveSide
 
 # 1.3 check diagonal
 def check_row_D():
 	global MoveSide, WinSide
-	if MoveSide == 'Black Turn':
-		if Board[0][0] + Board[1][1] + Board[2][2] == 2:
-			print(f'Decisive Move Deteced, Diagonal Row \ to be Completed by {MoveSide[0:5]}:')
-			for temp in range(3):
-				place_stone(temp, temp, 1)
-			WinSide = "Black"
-	else:
-		if Board[0][2] + Board[1][1] + Board[2][0] == 8:
-			print(f'Decisive Move Deteced, Diagonal Row / to be Completed by {MoveSide[0:5]}:')
-			for temp in range(3):
-				place_stone(temp, 2-temp, 4)
-			WinSide = "White"
+	value = get_value_by_side()
+	if sum(Board[i][i] for i in range(3)) == 2*value:
+		print(f'Decisive Move Deteced, Diagonal Row \ to be Completed by {MoveSide}:')
+		for temp in range(3):
+			if Board[temp][temp] == 0:
+				place_stone(temp, temp, value)
+		WinSide = MoveSide
+	elif sum(Board[i][2-i] for i in range(3)) == 2*value:
+		print(f'Decisive Move Deteced, Diagonal Row / to be Completed by {MoveSide}:')
+		for temp in range(3):
+			if Board[temp][2-temp] == 0:
+				place_stone(temp, temp, value)
+		WinSide = MoveSide
 
+
+# Level 2: Counter Rows
+# 2.1 check horizontal
+def counter_row_H():
+	global MoveSide, WinSide
+	value_opp = get_value_by_side('1')
+	value = get_value_by_side()
+	for x in range(board_height):
+		if sum(Board[x]) == 2*value_opp:
+			print(f'Decisive Move to be Countered, Horizontal Row {x} to be Completed by {MoveSide}:')
+			for index, row_item in enumerate(Board[x]):
+				if row_item == 0:
+					place_stone(x, index, value)
 
 
 
@@ -108,12 +124,7 @@ def RandomMove():
 		return
 
 	choice = random.choice(random_space)
-	print(f'{MoveSide[0:5]} is going to place at {choice}')
-
-	if MoveSide == 'Black Turn':
-		place_stone(int(choice[0]), int(choice[1]), 1)
-	else:
-		place_stone(int(choice[0]), int(choice[1]), 4)
+	place_stone(int(choice[0]), int(choice[1]), get_value_by_side())
 
 
 
@@ -123,8 +134,9 @@ board_width, board_height = 3, 3;
 Board = [[0 for x in range(board_width)] for y in range(board_height)]
 print('Board Initialization Complete')
 
-MoveSide = 'Black Turn'
+MoveSide = 'Black'
 WinSide = ''
+Placed = False
 
 # Game begin
 
@@ -142,6 +154,9 @@ while(WinSide == ''):
 		check_row_D()
 	
 	if WinSide == '':
+		counter_row_H()
+
+	if WinSide == '' and not Placed:
 		RandomMove()
 	
 	if WinSide != '':
@@ -157,3 +172,4 @@ while(WinSide == ''):
 
 	print_board()
 	switch_player()
+	Placed = False
