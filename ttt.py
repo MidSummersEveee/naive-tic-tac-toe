bc0adf77f63cf66e9166e4784a5d82b7ced897f6
Ian Jiang <midsummerseve1@gmail.com>
2018-01-23 12:48:21 -0500

    Refactoring Done, Winner Logic Partially Done

--- ttt.py
+++ ttt.py
@@ -2,41 +2,32 @@
 # Attempt on implementing a naive tic-tac-toe
 import random
 
-# Board Initialization
-board_width, board_height = 3, 3;
-Board = [[0 for x in range(board_width)] for y in range(board_height)]
-print('Board Initialization Complete')
 
-MoveSide = 'Black Turn'
-
-# NOTE: THE FUNCTION MUST BE CALLED AFTER CURRENT PLAYER PLAYCING
-# THEIR STONES
-def set_move_side(side):
-	print_board()
+# Switching Player
+def switch_player():
 	global MoveSide
-	MoveSide = side
-	print(side)
+	if(MoveSide == 'White Turn'):
+		MoveSide = 'Black Turn'
+	else:
+		MoveSide = 'White Turn'
+	print(MoveSide)
 
 
 def place_stone(x, y, value):
 	Board[x][y] = value
 
 # Attempts on Printing the Board
-# [[print(Board[x][y], end='') for x in range(board_width)] for y in range(board_height)]
-
 # for x in range(board_height):
 # 	[print(Board[x][y], end='') if y < 2 else print(Board[x][y]) for y in range(board_width)]
-
 def print_board():
 	[[print(Board[x][y], end=' ') if y < 2 else print(Board[x][y]) for y in range(board_width)] for x in range(board_height)]
-
 
 
 
 # Level 1: Check Rows
 # 1.1 check horizontal
 def check_row_H():
-	global MoveSide
+	global MoveSide, WinSide
 	# print('Display Horizontal Sums')
 	# [print(sum(Board[x])) for x in range(board_height)]
 	if MoveSide == 'Black Turn':
@@ -44,13 +35,13 @@ def check_row_H():
 			if sum(Board[x]) == 2:
 				print(f'Decisive Move Deteced, Horizontal Row {x} to be Completed by {MoveSide[0:5]}:')
 				Board[x] = [1, 1, 1]
-				set_move_side('White Turn')
+				WinSide = "Black"
 	else:
 		for x in range(board_height):
 			if sum(Board[x]) == 8:
 				print(f'Decisive Move Deteced, Horizontal Row {x} to be Completed by {MoveSide[0:5]}:')
 				Board[x] = [4, 4, 4]
-				set_move_side('Black Turn')
+				WinSide = "White"
 
 
 
@@ -66,6 +57,7 @@ def check_board_has_empty():
 	else:
 		return True
 
+
 # Random Placing Method
 def RandomMove():
 	global MoveSide
@@ -75,7 +67,7 @@ def RandomMove():
 		for y in range(board_width):
 			if Board[x][y] < 1:
 				random_space.append("".join([str(x), str(y)]))
-	print(random_space)
+	# print(random_space)
 	if len(random_space) == 0:
 		print("Game Over")
 		return
@@ -85,29 +77,42 @@ def RandomMove():
 
 	if MoveSide == 'Black Turn':
 		place_stone(int(choice[0]), int(choice[1]), 1)
-		set_move_side('White Turn')
 	else:
 		place_stone(int(choice[0]), int(choice[1]), 4)
-		set_move_side('Black Turn')
 
 
+
+
+# Board Initialization
+board_width, board_height = 3, 3;
+Board = [[0 for x in range(board_width)] for y in range(board_height)]
+print('Board Initialization Complete')
+
+MoveSide = 'Black Turn'
+WinSide = ''
 
 # Game begin
 
-Board[0][0] = 1
-Board[0][2] = 1
-Board[1][0] = 1
+# Board[0][0] = 1
+# Board[0][2] = 1
+# Board[1][0] = 1
 
-# print_board()
-
-set_move_side('Black Turn')
-
-
-# check_row_H()
-
-# RandomMove()
-
-while(check_board_has_empty()):
+while(WinSide == ''):
 	check_row_H()
-	RandomMove()
-print("All places have been occupied, Game Over!")
+	
+	if WinSide == '':
+		RandomMove()
+	
+	if WinSide != '':
+		print_board()
+		print(f'Game Over! {WinSide} Win !')
+		break
+	
+	if not check_board_has_empty():
+		WinSide = 'Draw'
+		print_board()
+		print(f'Game Over! {WinSide}!')
+		break
+
+	print_board()
+	switch_player()
